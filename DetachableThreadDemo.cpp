11c28cdfe837542bfa3d312363eed54358ab50e0
root <itskirancs7@gmail.com>
2016-12-19 03:03:24 -0500

    Added Detachable Thread Demo

---
+++ DetachableThreadDemo.cpp
@@ -0,0 +1,69 @@
+#include "DetachableThreadDemo.h"
+
+extern "C" void* startNotifRegThreadRoutine(void* data)
+{
+    return NotificationRegistry::start_notif_registration_thread(data);
+}
+
+
+void* NotificationRegistry::start_notif_registration_thread(void* data)
+{
+   cout<<"Inside thread function"<<(*(char*)(data))<<endl;
+   /* Add all Your LogicHere*
+      Below Example runs a system command and parses the output.
+   */
+   NotificationRegistry::RunCmd("ls");
+}
+
+bool NotificationRegistry::createThreadforNotifReg(StartMethod method, void* data)
+{
+	cout<<"Here";
+    bool return_code = true;
+    assert(method);
+    pthread_smart_attr smart_attr(PTHREAD_CREATE_DETACHED);
+    int result = pthread_create(&m_thread_notif_reg, smart_attr.get_pthread_attr(), method, (void*) data);
+    if (result)
+    {
+        cout<<" createThreadforNsrCache failed and return code from pthread_create() = " << result;
+        return_code = false;
+    }
+
+    return false;
+}
+void NotificationRegistry::RunCmd(string& cmd) {
+
+    FILE *in;
+    char buff[512];
+
+    if(!(in = popen(cmd.c_str(), "r"))){
+        return;
+    }
+
+    while(fgets(buff, sizeof(buff), in) != NULL)
+    {
+        cout << buff;
+    }
+    pclose(in);
+
+ }
+void NotificationRegistry::start_thread()
+{
+
+    char* l_ptr = new char[100];
+    l_ptr[100]= {'\0'};
+    strcpy(l_ptr,"---D-E-T-A-C-H-A-B-L-E T-H-R-E-A-D---");
+    bool return_code = createThreadforNotifReg(startNotifRegThreadRoutine, l_ptr);
+    delete l_ptr;
+}
+
+int main()
+{
+  NotificationRegistry nr;
+  nr.start_thread();
+  while(true)
+  {
+	  usleep(5);
+  }/*Assumption Main thread Won't Stop in Real Time untill forcefull shutdown is performed
+     Mainthread lifetime is more than worker thread duration.
+    */
+}
---
+++ DetachableThreadDemo.h
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<pthread.h>
+#include<string>
+#include<algorithm>
+#include<assert.h>
+#include<string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include<stdio.h>
+
+using namespace std;
+
+extern "C" typedef  void* (* StartMethod)(void*);
+
+ class pthread_smart_attr
+ {
+  private:
+  pthread_attr_t m_attr;
+
+  public:
+
+   pthread_smart_attr(unsigned int thread_type)
+   {
+      pthread_attr_init(&m_attr);
+      pthread_attr_setdetachstate(&m_attr, thread_type);
+   }
+   ~pthread_smart_attr()
+   {
+      //Free attribute
+      pthread_attr_destroy(&m_attr);
+   }
+   pthread_attr_t* get_pthread_attr()
+   {
+      return &m_attr;
+   }
+ };
+
+ class NotificationRegistry
+ {
+
+    public:
+        static void* start_notif_registration_thread(void* data);
+		static void RunCmd(string& cmd);
+        void start_thread();
+
+    private:
+     pthread_t m_thread_notif_reg;
+     pthread_mutex_t m_mutex;
+     bool createThreadforNotifReg(StartMethod method, void* data);
+
+  };
+
